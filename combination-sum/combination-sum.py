65def22fac6ed1263f374e72eb755edeed4d9633
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-23 15:26:03 -0500

    feat: update python3 solution for combination-sum

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -6,13 +6,13 @@ class Solution:
             if current_sum > target:
                 return
             if current_sum == target:
-                res.append(combination)
+                res.append(combination.copy())
                 return
             # current_sum < target:
             for i in range(index, len(candidates)):
-                # combination.append(num)
                 num = candidates[i]
-                dfs(combination + [num], current_sum + num, i)
-                # combination.pop(-1)
+                combination.append(num)
+                dfs(combination, current_sum + num, i)
+                combination.pop(-1)
         dfs([], 0, 0)
         return res
