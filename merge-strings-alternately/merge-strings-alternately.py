01fe0ca21c3da492b79ff76150cf56b84e5ba70b
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-22 22:34:28 -0500

    feat: update python3 solution for merge-strings-alternately

--- merge-strings-alternately/merge-strings-alternately.py
+++ merge-strings-alternately/merge-strings-alternately.py
@@ -2,15 +2,11 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         i = j = 0 # i is pointer for word1, j is pointer for word 2
         res = ""
-        while i < len(word1) and j < len(word2):
-            if len(res) % 2 == 0:
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
                 res += word1[i]
                 i += 1
-            else:
+            if j < len(word2):
                 res += word2[j]
                 j += 1
-        if i < len(word1):
-            res += word1[i:]
-        if j < len(word2):
-            res += word2[j:]
         return res
