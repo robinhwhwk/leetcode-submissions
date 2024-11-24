a480b942a624d985baa05ce7261fbdaa1690b9ae
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-23 21:04:16 -0500

    feat: update python3 solution for design-add-and-search-words-data-structure

--- design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -17,23 +17,23 @@ class WordDictionary:
         curr.word = True
 
     def search(self, word: str) -> bool:
-        def search_subtree(root, word, index):
-            if index == len(word):
+        def search_subtree(root, word):
+            if not word:
                 return root.word
-            ch = word[index]
+            ch = word[0]
             if ch == ".":
                 for child in root.children.values(): # child is a trie node
                     # check if this path has word
-                    if search_subtree(child, word, index + 1):
+                    if search_subtree(child, word[1:]):
                         return True
                 return False
             elif ch not in root.children:
                 return False
             else:
                 root = root.children[ch]
-                return search_subtree(root, word, index + 1)
+                return search_subtree(root, word[1:])
 
-        return search_subtree(self.trie, word, 0)
+        return search_subtree(self.trie, word)
 
 
 # Your WordDictionary object will be instantiated and called as such:
