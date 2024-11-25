5fae89ba17276342f67fe0508b166706086a4da1
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-25 12:43:03 -0500

    feat: update python3 solution for valid-palindrome-ii

--- valid-palindrome-ii/valid-palindrome-ii.py
+++ valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,14 +1,7 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         def isPalindrome(s):
-            start, end = 0, len(s) - 1
-            while start < end:
-                if s[start] == s[end]:
-                    start += 1
-                    end -= 1
-                else:
-                    return False
-            return True
+            return s == s[::-1]
         
         start, end = 0, len(s) - 1
         while start < end:
