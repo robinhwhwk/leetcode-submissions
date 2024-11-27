e5e49d065bf708947873cb4b212e7e7f8c969f7c
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-27 00:02:25 -0500

    feat: update python3 solution for top-k-frequent-elements

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,15 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # heap
-        count = dict()
+        # get frequency of each num
+        # sort them into buckets of frequency
+        # iterate buckets from the end, stop when we reach k items
+        frequency_map = dict()
         for num in nums:
-            if num not in count:
-                count[num] = 0
-            count[num] += 1
-        priority_queue = [(-prio, label) for label, prio in count.items()]
-        heapq.heapify(priority_queue)
-        res = []
-        for i in range(k):
-            top = heapq.heappop(priority_queue)
-            res.append(top[1])
+            if num not in frequency_map:
+                frequency_map[num] = 0
+            frequency_map[num] += 1
+        buckets = [[] for _ in range(len(nums) + 1)] # len(nums) + 1 b/c if we have an array of length 5 we want to have frequencies from 0 to 5
+        for key in frequency_map:
+            frequency = frequency_map[key]
+            buckets[frequency].append(key)
+        res = [0 for _ in range(k)]
+        index = 0
+        for frequency in range(len(nums), -1, -1):
+            # process each bucket at frequency
+            for key in buckets[frequency]:
+                res[index] = key
+                index += 1
+                if index == k:
+                    return res
         return res
