34404432137d7596642cf861878ec8b8aa34b8ff
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-28 01:19:26 -0500

    feat: add python3 solution for best-time-to-buy-and-sell-stock

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            profit = price - buy_price
+            max_profit = max(profit, max_profit)
+            if price < buy_price:
+                buy_price = price
+        return max_profit
+            
