3952e92493929978d5da52e8278c5957ed38e286
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-11-28 12:44:08 -0500

    feat: update python3 solution for permutation-in-string

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -2,19 +2,21 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        alphabet_count = [0 for _ in range(26)]
-        for ch in s1:
-            alphabet_count[ord(ch) - ord('a')] += 1
-        left = 0
-        for right in range(len(s2)):
-            # subtract 1 from s2[right]
-            alphabet_count[ord(s2[right]) - ord('a')] -= 1
-            # move left up until substring doesn't contain more chars than needed 
-            while left < right and min(alphabet_count) < 0:
-                alphabet_count[ord(s2[left]) - ord('a')] += 1
-                left += 1
-            if not any(alphabet_count):
-                return True
-            
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
 
+        left, right = 0, len(s1) - 1
+        while right < len(s2) - 1:
+            # check the substring from [left, right] inclusive
+            if s1_count == s2_count:
+                return True
+            s2_count[ord(s2[left]) - ord('a')] -= 1
+            left += 1
+            right += 1
+            s2_count[ord(s2[right]) - ord('a')] += 1
+            if s1_count == s2_count:
+                return True
         return False
