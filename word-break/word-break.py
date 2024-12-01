a80256faec2b72fa95733a11a34b677fea224f4e
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-01 12:46:30 -0500

    feat: add python3 solution for word-break

---
+++ word-break/word-break.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # 1-d dp solution
+        # iterate through the string
+        # if current string matches a word, set it to true
+        # for each word in wordDict, check if current_string - word is true
+        dp = [False for _ in range(len(s) + 1)]
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for word in wordDict:
+                if word == s[i - len(word):i] and dp[i - len(word)]:
+                    dp[i] = True
+        return dp[len(s)]
