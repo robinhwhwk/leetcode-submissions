48f442b887f5b3b5e3c34fb5c40cf083ccf14cf7
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-02 13:43:33 -0500

    feat: add python3 solution for car-fleet

---
+++ car-fleet/car-fleet.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        times = [[position[i], int((target-position[i])/speed[i])] for i in range(len(position))]
+        times.sort() # sorted by position from smallest
+        res = 1
+        slowest_time = times[-1][1]
+        for i in range(len(times) - 2, -1, -1):
+            if times[i][1] > slowest_time:
+                slowest_time = times[i][1]
+                res += 1
+        return res
+
+       
