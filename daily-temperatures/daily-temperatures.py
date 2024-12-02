50307d5aa1b2530b89efbb6d260859a88f149287
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-02 13:10:51 -0500

    feat: update python3 solution for daily-temperatures

--- daily-temperatures/daily-temperatures.py
+++ daily-temperatures/daily-temperatures.py
@@ -3,12 +3,14 @@ class Solution:
         # keep a stack of tuples: (temperature, index)
         # once we encounter a new temp, pop all from stack with temp < curr
         # when we pop, set arr[index] = currIndex - index
-        stack = []
+        stack = [0 for _ in range(len(temperatures))]
         res = [0 for _ in range(len(temperatures))]
+        top = -1
         for i in range(len(temperatures)):
             current_temp = temperatures[i]
-            while stack and current_temp > stack[-1][0]:
-                temp, index = stack.pop()
-                res[index] = i - index
-            stack.append([current_temp, i])
+            while top != -1 and current_temp > temperatures[stack[top]]:
+                res[stack[top]] = i - stack[top]
+                top -= 1
+            top += 1
+            stack[top] = i
         return res
