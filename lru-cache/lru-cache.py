e87880cd30b5a1d6a51439f6529273829858480c
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 17:31:50 -0500

    feat: update python3 solution for lru-cache

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,65 +1,60 @@
-class ListNode:
-    def __init__(self, key=None, val=None, next=None):
-        self.key = key
-        self.val = val
-        self.next = next
+class Node:
+    def __init__(self):
+        self.key = None
+        self.val = None
+        self.next = None
+        self.prev = None
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.head = ListNode() # head.next is the LRU
-        self.tail = ListNode() # tail.prev is the MRU
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.cache = dict() # key : ListNode()
         self.capacity = capacity
         self.size = 0
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.cache = dict()
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            # update this as the MRU, 1 before the tail
-            target = self.cache[key]
-            target.prev.next = target.next
-            target.next.prev = target.prev
-            self.tail.prev.next = target
-            target.prev = self.tail.prev
-            target.next = self.tail
-            self.tail.prev = target
-            return target.val
+            node = self.cache[key]
+            self.remove(node)
+            self.add(node)
+            return node.val
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache: # already there, no need to evict
+        if key in self.cache:
             node = self.cache[key]
+            self.remove(node)
             node.val = value
-            node.prev.next = node.next
-            node.prev.next.prev = node.prev
-            self.tail.prev.next = node
-            node.prev = self.tail.prev
-            node.next = self.tail
-            self.tail.prev = node
-        else: 
-            if self.size == self.capacity:
-                # at capacity, evict LRU and insert at MRU
-                target = self.head.next
-                self.head.next = self.head.next.next
-                if target.next:
-                    self.head.next.prev = self.head
-                del self.cache[target.key]
-                self.size -= 1
-            node = ListNode(key, value)
-            self.tail.prev.next = node
-            node.prev = self.tail.prev
-            self.tail.prev = node
-            node.next = self.tail
+            self.add(node)
+        else:
+            node = Node()
+            node.key = key
+            node.val = value
+            self.add(node)
             self.cache[key] = node
             self.size += 1
+        if self.size > self.capacity:
+            lru = self.head.next
+            del self.cache[lru.key]
+            self.remove(self.head.next)
+            self.size -= 1
 
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
 
+    def add(self, node):
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
 
-
-
-
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
