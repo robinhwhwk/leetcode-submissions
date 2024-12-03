62f1a29c76dfc7b6ea437a02f106a42a688d4b5c
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 14:01:00 -0500

    feat: add python3 solution for candy

---
+++ candy/candy.py
@@ -0,0 +1,11 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies = [1 for _ in range(len(ratings))]
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        for j in range(len(ratings) - 2, -1, -1):
+            if ratings[j] > ratings[j + 1]:
+                candies[j] = max(candies[j], candies[j + 1] + 1)
+
+        return sum(candies)
