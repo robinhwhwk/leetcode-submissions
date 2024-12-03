a400ea5a53a32659f68f910ee542b24627a4e483
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 11:43:05 -0500

    feat: update python3 solution for meeting-rooms-ii

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,22 +1,14 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # if two intervals are overlapping, then we need 2 rooms
-        # find the max number of intervals that overlap at any given time
-        # check every time period from [min(intervals[0]), max(intervals[1])] inclusive
-        min_start = float('inf')
-        max_end = 0
+        # sort the intervals by start time
+        times = []
         for interval in intervals:
-            min_start = min(interval[0], min_start)
-            max_end = max(interval[1], max_end)
-
-        max_count = 0
-        for interval in intervals:
-            t = interval[0]
-            count = 0
-            for interval in intervals:
-                if interval[0] <= t and interval[1] > t:
-                    count += 1
-            max_count = max(max_count, count)
-        return max_count
-
-
+            times.append([interval[0], 1])
+            times.append([interval[1], -1])
+        times.sort()
+        res = 0
+        curr = 0
+        for i in range(len(times)):
+            curr += times[i][1]
+            res = max(res, curr)
+        return res
