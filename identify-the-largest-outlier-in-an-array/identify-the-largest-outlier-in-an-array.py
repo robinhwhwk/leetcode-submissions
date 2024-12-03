111cc0289d019bc4e9e61b0d9738fe97753aa1c9
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 13:45:32 -0500

    feat: add python3 solution for identify-the-largest-outlier-in-an-array

---
+++ identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        uniques = set(nums)
+        counts = dict()
+        for num in nums:
+            if num not in counts:
+                counts[num] = 0
+            counts[num] += 1
+        total = sum(nums)
+        outliers = []
+        for num in nums:
+            sum_without = total - num
+            if sum_without % 2 == 1:
+                continue
+            target = sum_without // 2
+            if num != target and target in counts:
+                outliers.append(num)
+            elif num == target and target in counts and counts[target] > 1:
+                outliers.append(num)
+
+        return max(outliers)
