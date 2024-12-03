a7bb7c2554dc9986cece046cfb713709d6e0dc8f
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 17:19:48 -0500

    feat: update python3 solution for merge-intervals

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -3,9 +3,9 @@ class Solution:
         intervals.sort() # sort by end
         stack = []
         for interval in intervals:
-            while stack and stack[-1][1] >= interval[0]:
+            if stack and stack[-1][1] >= interval[0]:
                 newEnd = max(stack[-1][1], interval[1])
-                interval = [stack[-1][0], newEnd]
-                stack.pop()
-            stack.append(interval)
+                stack[-1][1] = newEnd
+            else:
+                stack.append(interval)
         return stack
