475cdb4f6350768f964115306925c18e6290c466
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 12:19:48 -0500

    feat: update python3 solution for number-of-flowers-in-full-bloom

--- number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
+++ number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
@@ -2,16 +2,16 @@ class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
         events = []
         for flower in flowers:
-            events.append((flower[0], 1))
-            events.append((flower[1] + 1, -1))
+            events.append((flower[0], 0, 1))
+            events.append((flower[1], 2, -1))
         for i in range(len(people)):
-            events.append((people[i], 2, i))
+            events.append((people[i], 1, i))
         events.sort()
         res = [0 for _ in range(len(people))]
         count = 0
         for event in events:
-            if event[1] != 2:
-                count += event[1]
+            if event[1] != 1:
+                count += event[2]
             else:
                 res[event[2]] = count
         return res
