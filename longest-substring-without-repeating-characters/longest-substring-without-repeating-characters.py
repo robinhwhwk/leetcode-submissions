e3fe7a2ac95cd2dd002fe4fd334a011523486ec8
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-03 14:11:32 -0500

    feat: update python3 solution for longest-substring-without-repeating-characters

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,20 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # keep last occurence for each character
-        # increment if current character is not in last occurence
-        # if current character is in last occurence:
-        #   move start pointer to 1 + last encountered[char]
         last_occurence = dict()
-        start = end = max_len = 0
-        current_len = 0
+        longest_length = 0
+        start = 0
         for end in range(len(s)):
             if s[end] in last_occurence:
-                start = max(last_occurence[s[end]] + 1, start)
-            current_len = end - start + 1
-            max_len = max(max_len, current_len)
+                start = max(start, last_occurence[s[end]] + 1)
             last_occurence[s[end]] = end
-        return max_len
+            longest_length = max(longest_length, end - start + 1)
 
-
-
-
+            last_occurence[s[end]] = end
+        return longest_length
