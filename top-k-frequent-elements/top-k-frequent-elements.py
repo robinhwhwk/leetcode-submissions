3dd53e36d9daf65d1a494314bc239600f60b7172
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-06 00:04:02 -0500

    feat: update python3 solution for top-k-frequent-elements

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # brute force: heap and hashmap
+        # bucket sort: O(n)
+        buckets = [[] for _ in range(len(nums) + 1)] # freq from 0 to len(nums)
         frequency_map = dict()
         for i in range(len(nums)):
             if nums[i] not in frequency_map:
                 frequency_map[nums[i]] = 0
             frequency_map[nums[i]] += 1
-        frequency_heap = [(-freq, val) for val, freq in frequency_map.items()]
-        heapq.heapify(frequency_heap)
+        for freq, value in frequency_map.items():
+            buckets[freq].append(value)
         res = []
-        for j in range(k):
-            res.append(heapq.heappop(frequency_heap)[1])
+        for i in range(len(nums), -1, -1): # scan frequency buckets from backwards
+            current_bucket = buckets[i]
+            j = 0
+            while k > 0 and j < len(buckets[i]):
+                res.append(buckets[i][j])
+                j += 1
+                k -= 1
+            if k == 0:
+                break
         return res
+
