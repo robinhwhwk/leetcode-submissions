05a4a2bdcbf160f0bae10ae647f18cd34d6f9f87
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-06 14:33:25 -0500

    feat: update python3 solution for powx-n

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,8 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        res = 1
-        for i in range(abs(n)):
-            res *= x
+        def dfs(num, exponent):
+            if exponent == 0:
+                return 1
+            if exponent == 1:
+                return num
+            if exponent % 2 == 0:
+                left = dfs(num, exponent // 2)
+                return left * left
+            else:
+                return dfs(num, exponent // 2 + 1) * dfs(num, exponent // 2)
+
+        res = dfs(x, abs(n))
         if n < 0:
             res = 1 / res
         return res
