09016af4c6d6e258b0325a1c32f9bf60c4fb11e9
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-06 14:27:53 -0500

    feat: update python3 solution for powx-n

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,3 +1,10 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x, n)
+        res = 1
+        for i in range(abs(n)):
+            res *= x
+        if n < 0:
+            res = 1 / res
+        return res
+        
+
