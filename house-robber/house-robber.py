9851d6e2f4459ad4365ef012dc43166c1eaf82bd
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-11 15:46:06 -0500

    feat: add python3 solution for house-robber

---
+++ house-robber/house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # T(n) = max(T(n-1), T(n-2) + A[n])
+        # base case: T(0) = A[0]
+        # T(1) = max(A[0], A[1])
+        if len(nums) < 3:
+            return max(nums)
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[len(nums) - 1]
