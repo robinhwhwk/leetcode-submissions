3240069a8ba744ed00a189582e3a03d9f07096a2
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-12 15:51:21 -0500

    feat: update python3 solution for trapping-rain-water

--- trapping-rain-water/trapping-rain-water.py
+++ trapping-rain-water/trapping-rain-water.py
@@ -1,18 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-        max_left = max_right = 0
-        res = 0
-        while left < right:
-            if height[left] < height[right]:
-                if max_left > height[left]:
-                    res += max_left - height[left]
-                max_left = max(max_left, height[left])
-                left += 1
-            else:
-                if max_right > height[right]:
-                    res += max_right - height[right]
-                max_right = max(max_right, height[right])
-                right -= 1
-            
-        return res
+        max_left = [0 for _ in range(len(height))]
+        for i in range(1, len(height)):
+            max_left[i] = max(max_left[i - 1], height[i - 1])
+        max_right = [0 for _ in range(len(height))]
+        for j in range(len(height) - 2, -1, -1):
+            max_right[j] = max(max_right[j + 1], height[j + 1])
+        # now we have 2 arrays, max_left and max_right that contains max height for left and right for each index
+        total = 0
+        for k in range(len(height)):
+            min_left_right = min(max_left[k], max_right[k])
+            if height[k] < min_left_right:
+                total += min_left_right - height[k]
+            print("index", k)
+            print(total)
+        return total
