8b02168a7d9b1214ae90b06aa2b6a11c55807807
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-13 11:10:19 -0500

    feat: update python3 solution for lru-cache

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,24 +1,25 @@
 class Node:
-    def __init__(self):
-        self.key = None
-        self.val = None
+    def __init__(self, key=None, val=None):
+        self.key = key
+        self.val = val
         self.next = None
         self.prev = None
-        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.size = 0
+        self.hashmap = dict()
         self.head = Node()
         self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.cache = dict()
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
+        if key in self.hashmap:
+            # update this node to MRU
+            node = self.hashmap[key]
             self.remove(node)
             self.add(node)
             return node.val
@@ -26,35 +27,34 @@ class LRUCache:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
+        if key in self.hashmap:
+            node = self.hashmap[key]
             self.remove(node)
             node.val = value
             self.add(node)
         else:
-            node = Node()
-            node.key = key
-            node.val = value
+            if self.size == self.capacity:
+                # evict LRU
+                lru_node = self.head.next
+                self.remove(self.head.next)
+                del self.hashmap[lru_node.key]
+                self.size -= 1
+            node = Node(key, value)
+            self.hashmap[key] = node
             self.add(node)
-            self.cache[key] = node
             self.size += 1
-        if self.size > self.capacity:
-            lru = self.head.next
-            del self.cache[lru.key]
-            self.remove(self.head.next)
-            self.size -= 1
-
+        
+    # Add a node to the linked list as the MRU.
+    def add(self, node):
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev.next = node
+        self.tail.prev = node
+    
+    # Remove a node from the linked list.
     def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
-
-    def add(self, node):
-        self.tail.prev.next = node
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev = node
-
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
