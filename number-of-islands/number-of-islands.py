cff54a24f890e62743f3635f3286906e48f07b13
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-12 23:32:23 -0500

    feat: update python3 solution for number-of-islands

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,18 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # process islands
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
+        def dfs(row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] != "1":
                 return
-            grid[i][j] = "0"
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
+            grid[row][col] = "#"
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
         res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     res += 1
-                    dfs(i, j)
+                dfs(i, j)
         return res
+        
