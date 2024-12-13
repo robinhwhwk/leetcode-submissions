6c6f16ea8291868451afaadf4827a83b5e516d3d
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-12 23:46:11 -0500

    feat: update python3 solution for group-anagrams

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -2,8 +2,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = dict()
         for s in strs:
-            hash_string = "".join(sorted(s))
-            if hash_string not in hashmap:
-                hashmap[hash_string] = []
-            hashmap[hash_string].append(s)
+            count = [0 for _ in range(26)]
+            for ch in s:
+                count[ord(ch) - ord('a')] += 1
+            hash_tuple = tuple(count)
+            if hash_tuple not in hashmap:
+                hashmap[hash_tuple] = []
+            hashmap[hash_tuple].append(s)
         return list(hashmap.values())
