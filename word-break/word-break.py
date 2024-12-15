d6f7497b59a8fd8d2deb5ab1190524aa1893a422
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-15 13:41:03 -0500

    feat: update python3 solution for word-break

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -2,12 +2,11 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False for _ in range(len(s) + 1)]
         dp[0] = True
-        for i in range(1, len(s) + 1):
-            for word in wordDict:
-                wordLen = len(word)
-                if wordLen > i:
-                    continue
-                if s[i - wordLen:i] == word and dp[i - wordLen]:
-                    dp[i] = True
+        wordSet = set(wordDict)
+        for i in range(len(s)):
+            for j in range(i, -1, -1):
+                word = s[j:i+1]
+                if word in wordSet and dp[j]:
+                    dp[i + 1] = True
                     break
         return dp[len(s)]
