dfa0d56419797ed4ae756294bc9ced6e7e5894ad
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-15 13:36:22 -0500

    feat: update python3 solution for word-break

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,15 +1,13 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # 1-d dp solution
-        # iterate through the string
-        # if current string matches a word, set it to true
-        # for each word in wordDict, check if current_string - word is true
         dp = [False for _ in range(len(s) + 1)]
         dp[0] = True
-        wordSet = set(wordDict)
         for i in range(1, len(s) + 1):
-            for j in range(i - 1, -1, -1):
-                if s[j:i] in wordSet and dp[j]:
+            for word in wordDict:
+                wordLen = len(word)
+                if wordLen > i:
+                    continue
+                if s[i - wordLen:i] == word and dp[i - wordLen]:
                     dp[i] = True
                     break
         return dp[len(s)]
