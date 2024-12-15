fb69c5068434910aa057311e781ed65a61190fff
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-15 15:13:57 -0500

    feat: update python3 solution for maximum-average-pass-ratio

--- maximum-average-pass-ratio/maximum-average-pass-ratio.py
+++ maximum-average-pass-ratio/maximum-average-pass-ratio.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         # compute the added value for each class
-        max_heap = [(-((p+1)/(t+1) - p/t), [p+1,t+1]) for p, t in classes]
+        max_heap = [(-((p+1)/(t+1) - p/t), p, t) for p, t in classes]
         heapq.heapify(max_heap)
         for i in range(extraStudents):
-            p, t = heapq.heappop(max_heap)[1]
-            added_value = -((p+1)/(t+1) - p/t)
-            heapq.heappush(max_heap, (added_value, [p + 1, t + 1]))
+            value, p, t = heapq.heappop(max_heap)
+            added_value = -((p+2)/(t+2) - (p+1)/(t+1))
+            heapq.heappush(max_heap, (added_value, p + 1, t + 1))
         total = 0
-        for added_value, (p, t) in max_heap:
-            total += (p-1)/(t-1)
+        for added_value, p, t in max_heap:
+            total += p/t
         return total / len(classes)
 
 
