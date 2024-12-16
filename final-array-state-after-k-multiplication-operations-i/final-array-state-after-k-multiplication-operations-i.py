2d97cc0c7882767c1fc2e98d7a737fdaee38219b
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2024-12-16 15:04:27 -0500

    feat: update python3 solution for final-array-state-after-k-multiplication-operations-i

--- final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -1,20 +1,12 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
         locations = dict()
-        result = nums.copy()
-        for i in range(len(nums)):
-            if nums[i] not in locations:
-                locations[nums[i]] = []
-            locations[nums[i]].append(i)
-        heapq.heapify(nums)
+        heap = [(x, i) for i, x in enumerate(nums)]
+        heapq.heapify(heap)
         
         for j in range(k):
-            min_element = heapq.heappop(nums)
-            min_location = heapq.heappop(locations[min_element])
-            new_element = multiplier * min_element
-            if new_element not in locations:
-                locations[new_element] = []
-            heapq.heappush(locations[new_element], min_location)
-            heapq.heappush(nums, new_element)
-            result[min_location] = new_element
-        return result
+            x, i = heap[0]
+            heapq.heapreplace(heap, (x * multiplier, i))
+        for x, i in heap:
+            nums[i] = x
+        return nums
